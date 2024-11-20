1734aea987f29944dc306453fa89af472a095906
ganznz <loganbellinghamlb@gmail.com>
2024-11-20 23:42:40 +1300

    changes to api config + igdbApiClient

--- server/config/apiConfig.ts
+++ server/config/apiConfig.ts
@@ -1,6 +1,9 @@
 export const API_CONFIG = {
     IGDB_API: {
-        BASE_URL: process.env.IGDB_AWS_PROXY_URL,
+        IGDB_CLIENT_ID: process.env.IGDB_CLIENT_ID,
+        IGDB_ACCESS_TOKEN: process.env.IGDB_ACCESS_TOKEN,
+        IGDB_PROXY_BASE_URL: process.env.IGDB_AWS_PROXY_URL,
+        IGDB_API_BASE_URL: `${process.env.IGDB_AWS_PROXY_URL}/v4`,
         X_API_KEY: process.env.IGDB_AWS_X_API_KEY,
     },
 };
--- server/services/igdb.ts
+++ server/services/igdb.ts
@@ -1,21 +1,11 @@
 import axios from "axios";
 import { API_CONFIG } from "../config/apiConfig.js";
 
-const igdbApiClient = axios.create({
-    baseURL: API_CONFIG.IGDB_API.BASE_URL,
+export const igdbApiClient = axios.create({
+    baseURL: API_CONFIG.IGDB_API.IGDB_API_BASE_URL,
     headers: {
+        Accept: "application/json",
+        "Content-Type": "text/plain",
         "x-api-key": API_CONFIG.IGDB_API.X_API_KEY,
     },
 });
-
-// test api call, delete later
-export const getGameData = async () => {
-    try {
-        const res = await igdbApiClient.get("/v4/games");
-        console.log(res.data);
-        return res.data;
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-};
