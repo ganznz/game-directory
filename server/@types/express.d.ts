8c71fa5794c3cd2c2bc5e72a8cbb33eb691a528d
ganznz <loganbellinghamlb@gmail.com>
2024-11-20 14:00:46 +1300

    added parsedQueryParams type for express' Express object

---
+++ server/@types/express.d.ts
@@ -0,0 +1,10 @@
+import { Request as ExpressRequest } from "express";
+import { ParsedQs } from "qs";
+
+declare global {
+    namespace Express {
+        interface Request {
+            parsedQueryParams?: ParsedQs;
+        }
+    }
+}
