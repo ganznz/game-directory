675fee30f783e4dd4e5ecae98546d2dcd8500036
ganznz <loganbellinghamlb@gmail.com>
2025-01-28 22:47:17 +1300

    remove mui

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 
 import stylesheet from "./app.css?url";
+import { NavBar } from "./components/composite/navbar";
 
 export const links: Route.LinksFunction = () => [
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -17,18 +18,6 @@ export const links: Route.LinksFunction = () => [
         rel: "preconnect",
         href: "https://fonts.gstatic.com",
         crossOrigin: "anonymous",
-    },
-    {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-    },
-    {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
-    },
-    {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/icon?family=Material+Icons",
     },
     { rel: "stylesheet", href: stylesheet },
 ];
@@ -57,6 +46,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
     return (
         <>
+            <NavBar />
             <Outlet />
         </>
     );
