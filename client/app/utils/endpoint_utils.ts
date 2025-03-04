d0389c3e367f155b5ec971b625f254a2a6900847
ganznz <loganbellinghamlb@gmail.com>
2025-03-04 17:13:04 +1300

    create ``adjustImageSize`` util func for changing size of idgb api images

--- client/app/utils/endpoint_utils.ts
+++ client/app/utils/endpoint_utils.ts
@@ -2,36 +2,36 @@
  * Enum representing available image size options for IGDB images
  * @enum {string}
  */
-enum ImageSize {
-    /** Cover small - 90x128px, Fit */
-    CoverSmall = "t_cover_small",
+export enum ImageSize {
+  /** Cover small - 90x128px, Fit */
+  CoverSmall = "t_cover_small",
 
-    /** Cover big - 264x374px, Fit */
-    CoverBig = "t_cover_big",
+  /** Cover big - 264x374px, Fit */
+  CoverBig = "t_cover_big",
 
-    /** Logo medium - 284x160px, Fit */
-    LogoMed = "t_logo_med",
+  /** Logo medium - 284x160px, Fit */
+  LogoMed = "t_logo_med",
 
-    /** Screenshot medium - 569x320px, Lfill, Center gravity */
-    ScreenshotMed = "t_screenshot_med",
+  /** Screenshot medium - 569x320px, Lfill, Center gravity */
+  ScreenshotMed = "t_screenshot_med",
 
-    /** Screenshot big - 889x500px, Lfill, Center gravity */
-    ScreenshotBig = "t_screenshot_big",
+  /** Screenshot big - 889x500px, Lfill, Center gravity */
+  ScreenshotBig = "t_screenshot_big",
 
-    /** Screenshot huge - 1280x720px, Lfill, Center gravity */
-    ScreenshotHuge = "t_screenshot_huge",
+  /** Screenshot huge - 1280x720px, Lfill, Center gravity */
+  ScreenshotHuge = "t_screenshot_huge",
 
-    /** Thumbnail - 90x90px, Thumb, Center gravity */
-    Thumb = "t_thumb",
+  /** Thumbnail - 90x90px, Thumb, Center gravity */
+  Thumb = "t_thumb",
 
-    /** Micro thumbnail - 35x35px, Thumb, Center gravity */
-    Micro = "t_micro",
+  /** Micro thumbnail - 35x35px, Thumb, Center gravity */
+  Micro = "t_micro",
 
-    /** 720p resolution - 1280x720px, Fit, Center gravity */
-    Resolution720 = "t_720p",
+  /** 720p resolution - 1280x720px, Fit, Center gravity */
+  Resolution720 = "t_720p",
 
-    /** 1080p resolution - 1920x1080px, Fit, Center gravity */
-    Resolution1080 = "t_1080p",
+  /** 1080p resolution - 1920x1080px, Fit, Center gravity */
+  Resolution1080 = "t_1080p",
 }
 
 /**
@@ -44,5 +44,5 @@ enum ImageSize {
  * const bigCover = adjustImageSize("https://images.igdb.com/...t_thumb.jpg", ImageSize.CoverBig);
  */
 export const adjustImageSize = (url: string, size: ImageSize): string => {
-    return url.replace("t_thumb", size);
+  return url.replace("t_thumb", size);
 };
